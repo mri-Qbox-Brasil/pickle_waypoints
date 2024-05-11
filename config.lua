f509173907de604659c6c2bc6982d3b2cb8bb691
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-05-11 03:31:48 -0300

    update pt-br

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {}
 
-Config.Language = "en" -- Language to use.
+Config.Language = "pt" -- Language to use.
 
-Config.UseImperial = true -- Use imperial units (feet/miles) instead of metric units (meters/kilometers).
+Config.UseImperial = false -- Use imperial units (feet/miles) instead of metric units (meters/kilometers).
 
 Config.RenderDistance = 300.0 -- When to render markers from waypoints.
 
@@ -11,7 +11,7 @@ Config.EnterDistance = 5.0 -- Distance from the waypoint to trigger the clear ev
 Config.WaypointSettings = { -- These are the default settings for the waypoint display. Players can change them on their end using the /waypointsettings command.
     icon = nil, -- Icon to use for the waypoint marker, set to nil to use the built-in waypoint icon. Using alternative images will prevent the color from being applied.
     enabled = true, -- Enable waypoint display.
-    color = {255, 255, 255, 200}, -- Color of the waypoint marker.
+    color = {255, 255, 255, 100}, -- Color of the waypoint marker.
 }
 
 Config.AdminWaypointPermissions = { -- These are the permissions required to set a waypoint for another player.
